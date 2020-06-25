7333c06518a9d121e89ee9312d88b6e3ad34a34f
Phil Schatz <253202+philschatz@users.noreply.github.com>
2020-06-25 07:45:11 -0500

    try using absolute paths again for codecov

--- mergeCoverageFiles.js
+++ mergeCoverageFiles.js
@@ -7,18 +7,18 @@ if (process.argv.length < 2 + 2) {
   process.exit(110)
 }
 
-// Make the paths relative. No need for unnecessary absolute paths
-function toRelative(coverage) {
-  const ret = {}
-  for (const entry of Object.values(coverage)) {
-    const relativized = path.relative(cwd, entry.path)
-    entry.path = relativized
-    ret[relativized] = entry
-  }
-  return ret  
-}
+// // Make the paths relative. No need for unnecessary absolute paths
+// function toRelative(coverage) {
+//   const ret = {}
+//   for (const entry of Object.values(coverage)) {
+//     const relativized = path.relative(cwd, entry.path)
+//     entry.path = relativized
+//     ret[relativized] = entry
+//   }
+//   return ret  
+// }
 
 let acc = {}
-process.argv.slice(2).forEach(c => acc = {...acc, ...toRelative(JSON.parse(fs.readFileSync(c)))})
+process.argv.slice(2).forEach(c => acc = {...acc, ...JSON.parse(fs.readFileSync(c))})
 
 console.log(JSON.stringify(acc))
